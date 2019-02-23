a9eb42af2e0b438d714abb19338aca5de24e4a0a
DeveloperArthur <42553658+DeveloperArthur@users.noreply.github.com>
2019-02-22 21:53:48 -0300

    Update ArrayAdress.c

--- ArrayAdress.c
+++ ArrayAdress.c
@@ -7,7 +7,7 @@ int main(){
 	char array[6];
 	printf("Endereco das variaveis do array:\n");
 	for(int i=0; i<6; i++){
-		printf("%p\n", &array[i]);
+		printf("Array[%d] = %p\n", i,&array[i]);
 	}
 	getch();
 	
