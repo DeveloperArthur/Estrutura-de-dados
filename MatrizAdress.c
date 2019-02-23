3a717e3176ec075ab706007282c7f6bbe6d7292c
DeveloperArthur <42553658+DeveloperArthur@users.noreply.github.com>
2019-02-22 21:54:07 -0300

    Update MatrizAdress.c

--- MatrizAdress.c
+++ MatrizAdress.c
@@ -8,7 +8,7 @@ int main(){
 	printf("Endereco das variaveis do array:\n");
 	for(int j=0; j<3; j++){
 		for(int i=0; i<2; i++){
-			printf("%p\n", &array[j][i]);
+			printf("Endereco da matriz[%d][%d] = %p\n", j,i,&array[j][i]);
 		}
 	}
 	getch();
