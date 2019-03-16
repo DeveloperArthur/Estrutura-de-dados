913e70734750f82537b791983407bac3e8421096
DeveloperArthur <42553658+DeveloperArthur@users.noreply.github.com>
2019-03-15 21:44:54 -0300

    Add files via upload

---
+++ Exercicio3.c
@@ -0,0 +1,15 @@
+/*Exercicio 3
+aça um programa que leia o nome de uma pessoa e depois
+apresente uma mensagem informando qual foi o nome digitado. */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+int main(){
+	char nome[20];
+	printf("Digite seu nome: ");
+	scanf("%s", &nome);
+	printf("Nome digitado: %s", nome);
+}
+
